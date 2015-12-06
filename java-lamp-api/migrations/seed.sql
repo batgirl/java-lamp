c68b245a2988caa00654c1efdc5d17f3432c6797
KevGary <kevgary.dev@gmail.com>
2015-12-06 15:46:55 -0700

    login, register, user endpoints.  db set up on local at java_lamp_db. schema.up schema.down seed migrations

---
+++ java-lamp-api/migrations/seed.sql
@@ -0,0 +1,9 @@
+INSERT INTO
+  users
+VALUES
+  ( default, 'Rachel Goof', 'rachelgimp@example.com', 'fruitloops', null ),
+  ( default, 'Richard Levy', 'richardlevy@example.com', 'down', null ),
+  ( default, 'Lynold Friedman', 'lynoldfriedman@example.com', 'up', null ),
+  ( default, 'Mary Cariah', 'marycariah@example.com', 'red', null ),
+  ( default, 'Tom Strongholde', 'tomstrongholde@example.com', 'blue', null ),
+  ( default, 'Sally Rallycaps', 'sallyrallycaps@example.com', 'green', null );
