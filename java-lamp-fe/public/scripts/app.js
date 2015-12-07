d5c78f18961327eacdf1d5cab7786c7087f1ce64
Chloe Alnaji <caedeal@gmail.com>
2015-12-06 17:59:34 -0700

    Added working error messages to Register

--- java-lamp-fe/public/scripts/app.js
+++ java-lamp-fe/public/scripts/app.js
@@ -2,6 +2,8 @@ angular.module('javaLamp', ['ngMaterial', 'ngMessages'])
   .controller('NavbarController', function($scope, $mdDialog, $mdMedia) {
     // $scope.status = "";
     $scope.showRegister = function(ev) {
+      // make Register tab md-active
+      $scope.selectedIndex = 0;
       $mdDialog.show({
         controller: DialogController,
         templateUrl:'partials/register-login-dialog.html',
@@ -12,6 +14,8 @@ angular.module('javaLamp', ['ngMaterial', 'ngMessages'])
     }
 
     $scope.showLogin = function(ev) {
+      // make Login tab md-active
+      $scope.selectedIndex = 1;
       $mdDialog.show({
         controller: DialogController,
         templateUrl:'partials/register-login-dialog.html',
