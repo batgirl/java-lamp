d4c5c766032e45b76cbd20741c597caa943ebacb
Chloe Alnaji <caedeal@gmail.com>
2015-12-06 22:38:50 -0700

    Fixed question text, removed second text editor

--- java-lamp-fe/public/scripts/ace_editor.js
+++ java-lamp-fe/public/scripts/ace_editor.js
@@ -2,10 +2,6 @@ var editor = ace.edit("editor");
 editor.setTheme("ace/theme/monokai");
 editor.getSession().setMode("ace/mode/javascript");
 
-var editor2 = ace.edit("editor2");
-editor2.setTheme("ace/theme/monokai");
-editor2.getSession().setMode("ace/mode/javascript");
-
 document.addEventListener('DOMContentLoaded', function() {
   console.log('connected to file');
 });
