cd81e42860b758953163f859c23867e84cf2075a
Chloe Alnaji <caedeal@gmail.com>
2015-12-09 16:47:51 -0700

    added ng-repeat for multiple questions

--- java-lamp-fe/public/scripts/ace_editor.js
+++ java-lamp-fe/public/scripts/ace_editor.js
@@ -1,7 +1,9 @@
-var editor = ace.edit("editor");
-editor.setTheme("ace/theme/monokai");
-editor.getSession().setMode("ace/mode/javascript");
+// $(document).ready(function() {
+  // if (editor) {
+  //   console.log(editor)
+  // }
+  var editor = ace.edit("editor");
+  editor.setTheme("ace/theme/monokai");
+  editor.getSession().setMode("ace/mode/javascript");
 
-document.addEventListener('DOMContentLoaded', function() {
-  console.log('connected to file');
-});
+// })
--- java-lamp-fe/public/scripts/app.js
+++ java-lamp-fe/public/scripts/app.js
@@ -1,4 +1,6 @@
-var app = angular.module('javaLamp', ['ngMaterial', 'ngMessages', 'ngRoute'])
+var app = angular.module('javaLamp', ['ui.ace', 'ngMaterial', 'ngMessages', 'ngRoute' ])
+
+app.constant('API_URL', 'http://localhost:3000');
 
 app.controller('NavbarController', function($rootScope, $scope, $mdDialog, $mdMedia) {
     // $scope.status = "";
@@ -34,9 +36,18 @@ app.controller('LandingController', function() {
 
 });
 
-app.controller('ChallengesController', function() {
+// app.run(['$anchorScroll', function($anchorScroll) {
+//   // $anchorScroll.yOffset = 400;   // always scroll by 50 extra pixels
+// }])
 
-});
+// app.run(function($rootScope, $location, $anchorScroll, $routeParams) {
+//   $rootScope.$on('$routeChangeSuccess', function(newRoute, oldRoute) {
+//     $location.hash();
+//     $anchorScroll();
+//   });
+// });
+
+
 
 function DialogController($rootScope, $scope, $mdDialog, $mdDialog) {
   $scope.selectedIndex = $rootScope.selectedIndex;
@@ -53,8 +64,9 @@ function DialogController($rootScope, $scope, $mdDialog, $mdDialog) {
 
 app.directive('aceEditor', function() {
   return {
+    scope: {},
     template: 
-    '<div id="editor">function foo() {\n\n}</div> <script type="text/javascript" src="/scripts/ace_editor.js"></script>'
+    '<div id="editor">function foo() {\n\n}</div><script type="text/javascript" src="/scripts/ace_editor.js">'
   }
 });
 
@@ -76,11 +88,170 @@ app.config(['$routeProvider', '$locationProvider', '$mdThemingProvider', functio
     });
   $locationProvider.html5Mode(true);
 
-  $mdThemingProvider.theme('default')
-    .primaryPalette('indigo')
-    .accentPalette('indigo')
-    .warnPalette('deep-orange');
+var customPrimary = {
+        '50': '#5e5aae',
+        '100': '#524fa0',
+        '200': '#49468f',
+        '300': '#403e7e',
+        '400': '#38356d',
+        '500': '#2F2D5C',
+        '600': '#26254b',
+        '700': '#1e1c3a',
+        '800': '#151429',
+        '900': '#0c0b17',
+        'A100': '#6e6bb7',
+        'A200': '#7f7cbf',
+        'A400': '#908ec7',
+        'A700': '#030306'
+    };
+    // $mdThemingProvider
+    //     .definePalette('customPrimary', 
+    //                     customPrimary);
 
-  $mdThemingProvider.theme('buttonTheme');
+    var customAccent = {
+        '50': '#f7f5f8',
+        '100': '#ebe7ed',
+        '200': '#ded8e2',
+        '300': '#d2c9d8',
+        '400': '#c5bbcd',
+        '500': '#B9ACC2',
+        '600': '#ad9db7',
+        '700': '#a08fac',
+        '800': '#9480a2',
+        '900': '#877197',
+        'A100': '#ffffff',
+        'A200': '#ffffff',
+        'A400': '#ffffff',
+        'A700': '#7b6589'
+    };
+    // $mdThemingProvider
+    //     .definePalette('customAccent', 
+    //                     customAccent);
+
+    var customWarn = {
+        '50': '#ccae7a',
+        '100': '#c5a367',
+        '200': '#be9855',
+        '300': '#b48c45',
+        '400': '#a17d3e',
+        '500': '#8F6F37',
+        '600': '#7d6130',
+        '700': '#6a5229',
+        '800': '#584422',
+        '900': '#45361b',
+        'A100': '#d3b98c',
+        'A200': '#dac49f',
+        'A400': '#e1d0b1',
+        'A700': '#332814'
+    };
+    // $mdThemingProvider
+    //     .definePalette('customWarn', 
+    //                     customWarn);
+
+    var customBackground = {
+        '50': '#aaa18b',
+        '100': '#9f957d',
+        '200': '#958a6e',
+        '300': '#867c63',
+        '400': '#786f58',
+        '500': '#69614D',
+        '600': '#5a5342',
+        '700': '#4c4637',
+        '800': '#3d382d',
+        '900': '#2e2b22',
+        'A100': '#b5ad9a',
+        'A200': '#c0b9a9',
+        'A400': '#cbc5b7',
+        'A700': '#1f1d17'
+    };
+   //  $mdThemingProvider
+   //      .definePalette('customBackground', 
+   //                      customBackground);
+
+   // $mdThemingProvider.theme('default')
+   //     .primaryPalette('customPrimary')
+   //     .accentPalette('customAccent')
+   //     .warnPalette('customWarn')
+   //     .backgroundPalette('customBackground');
   
 }]);
+
+app.controller('ChallengesController', function($scope, $location, $anchorScroll, DockerFactory) {
+  // $scope.isActive = true;
+
+  $scope.questions = [
+    {title: 'Is Unique',
+    questionText: 'Implement an algorithm that tests if each item in a string is unique.',
+    testSuite: 'test file',
+    testText: 'TEST WILL GO HERE',
+    answers: ['answer1', 'answer2']},
+    {title: 'Greatest Product',
+    questionText: 'Implement an algorithm to determine the greatest product between two elements in an array.',
+    testSuite: 'test file',
+    testText: 'TEST WILL GO HERE ALSO',
+    answers: ['answer3', 'answer4']},
+    {title: 'Cats Everywhere',
+    questionText: 'Tell us, why are there so many cats everywhere?',
+    testSuite: 'test file',
+    testText: 'TEST WILL GO HERE ALSO ALSO',
+    answers: ['answer5', 'answer6']}
+    ];
+
+    $scope.question_index = 0;
+
+    $scope.next = function() {
+      if ($scope.question_index >= $scope.questions.length-1) {
+        $scope.question_index = 0;
+      } else {
+        $scope.question_index++;
+      }
+    };
+
+    $scope.previous = function() {
+      if ($scope.question_index == 0) {
+        $scope.question_index = $scope.questions.length-1;
+      } else {
+        $scope.question_index--;
+      }
+    }
+
+    $scope.submit = function() {
+      console.log('submitted');
+      DockerFactory.dockerPost();
+    }
+
+  // $scope.$on('$locationChangeStart', function(ev) {
+  //   ev.preventDefault();
+  // });
+
+  $scope.showTest = false;
+  $scope.scrollTo = function(id, event) {
+    $scope.showTest = !$scope.showTest;
+    // event.preventDefault();
+    // event.stopPropagation();
+    // $anchorScroll.yOffset = 400;
+    $location.hash(id);
+    $anchorScroll();
+    $location.hash(null);
+    // if ($scope.showTest) {
+      // $scope.anchorId = id;
+      // var old = $location.hash();
+      // $location.hash(id);
+      // console.log($location.hash())
+      // $location.hash(old); 
+    // }
+    // $scope.apply();
+  };
+});
+
+app.factory('DockerFactory', function Docker($q, $http, API_URL) {
+  return {
+    dockerPost: dockerPost
+  }
+  function dockerPost() {
+    return $http.post(API_URL + '/docker', {"data": "hello"})
+    .then(function success(response) {
+      console.log("response: ", response);
+    })
+  }
+});
